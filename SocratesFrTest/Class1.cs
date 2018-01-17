9a4c61dc1e8960387fa3340998e90ddb3d77acbb
Arnaud Combellas <combellas.a@gmail.com>
2018-01-17 10:32:26 +0100

    Init repository

---
+++ SocratesFrTest/Class1.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace SocratesFrTest
+{
+    public class Class1
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.AreEqual(true, true);
+        }
+    }
+}
