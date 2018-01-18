9974bddd81278c0f50959330bb8ed3b0335a0bb0
Gabou <g.delarochette@live.fr>
2018-01-18 12:33:51 +0100

    Refactor test initilization

--- test/CandidateRegisterTest.java
+++ test/CandidateRegisterTest.java
@@ -1,80 +1,73 @@
 import org.assertj.core.api.Assertions;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class CandidateRegisterTest {
 
-    private CandidateRegister initializedCandidateRegister;
     public static final Candidate CAROLE = new Candidate("carole", "carole@gmail.com");
     public static final Candidate GABRIEL = new Candidate("gabriel", "gabriel@gmail.com");
+    public static final Candidate ANTHONY = new Candidate("anthony", "anthony@gmail.com");
     public static final Candidate ANTHONY_SAME_EMAIL_AS_CAROLE = new Candidate("anthony", "carole@gmail.com");
 
-    @Before
-    public void setUp() throws Exception {
-        List<Candidate> candidateList = new ArrayList<Candidate>();
-        candidateList.add(CAROLE);
-        initializedCandidateRegister = new CandidateRegister(candidateList);
+    private CandidateRegister initCandidateRegister(Candidate... candidates) {
+        List<Candidate> candidateList = new ArrayList<>();
+        candidateList.addAll(Arrays.asList(candidates));
+        return new CandidateRegister(candidateList);
     }
 
     @Test
-    public void retrieveEmptyList(){
-        CandidateRegister candidateRegister = new CandidateRegister();
+    public void retrieveEmptyList() {
+        CandidateRegister candidateRegister = initCandidateRegister();
         List<Candidate> candidateList = candidateRegister.retrieveAll();
         Assert.assertTrue(candidateList.isEmpty());
     }
 
     @Test
-    public void retrieveCandidateInList(){
-        List<Candidate> retrievedCandidateList = initializedCandidateRegister.retrieveAll();
+    public void retrieveCandidateInList() {
+        List<Candidate> retrievedCandidateList = initCandidateRegister(CAROLE).retrieveAll();
         Assertions.assertThat(retrievedCandidateList).containsExactly(CAROLE);
     }
 
     @Test
-    public void retrieveTwoCandidatesInList(){
-        List<Candidate> candidateList = new ArrayList<Candidate>();
-        candidateList.add(CAROLE);
-        candidateList.add(GABRIEL);
-        CandidateRegister candidateRegister = new CandidateRegister(candidateList);
+    public void retrieveTwoCandidatesInList() {
+        CandidateRegister candidateRegister = initCandidateRegister(CAROLE, GABRIEL);
         List<Candidate> retrievedCandidateList = candidateRegister.retrieveAll();
         Assertions.assertThat(retrievedCandidateList).containsExactly(CAROLE, GABRIEL);
     }
 
     @Test
-    public void addCandidateToEmptyList(){
-        CandidateRegister candidateRegister = new CandidateRegister();
+    public void addCandidateToEmptyList() {
+        CandidateRegister candidateRegister = initCandidateRegister();
         candidateRegister.addCandidate(CAROLE);
         List<Candidate> retrievedCandidateList = candidateRegister.retrieveAll();
         Assertions.assertThat(retrievedCandidateList).containsExactly(CAROLE);
     }
 
     @Test
-    public void addCandidateToNonEmptyList(){
-        initializedCandidateRegister.addCandidate(GABRIEL);
-        List<Candidate> retrievedCandidateList = initializedCandidateRegister.retrieveAll();
+    public void addCandidateToNonEmptyList() {
+        CandidateRegister candidateRegister = initCandidateRegister(CAROLE);
+        candidateRegister.addCandidate(GABRIEL);
+        List<Candidate> retrievedCandidateList = candidateRegister.retrieveAll();
         Assertions.assertThat(retrievedCandidateList).containsExactly(CAROLE, GABRIEL);
     }
 
     @Test
-    public void addCandidateWithSameEmail(){
-        initializedCandidateRegister.addCandidate(ANTHONY_SAME_EMAIL_AS_CAROLE);
-        List<Candidate> retrievedCandidateList = initializedCandidateRegister.retrieveAll();
+    public void addCandidateWithSameEmail() {
+        CandidateRegister candidateRegister = initCandidateRegister(CAROLE);
+        candidateRegister.addCandidate(ANTHONY_SAME_EMAIL_AS_CAROLE);
+        List<Candidate> retrievedCandidateList = candidateRegister.retrieveAll();
         Assertions.assertThat(retrievedCandidateList).containsExactly(CAROLE);
     }
 
     @Test
-    public void retrieveSortedCandidateList(){
-        initializedCandidateRegister.addCandidate(GABRIEL);
-        Candidate anthony = new Candidate("anthony", "anthony@gmail.com");
-        initializedCandidateRegister.addCandidate(anthony);
-        List<Candidate> retrievedCandidateList = initializedCandidateRegister.retrieveAll();
-        Assertions.assertThat(retrievedCandidateList).containsSequence(anthony, CAROLE, GABRIEL);
+    public void retrieveSortedCandidateList() {
+        List<Candidate> retrievedCandidateList = initCandidateRegister(CAROLE, GABRIEL, ANTHONY).retrieveAll();
+        Assertions.assertThat(retrievedCandidateList).containsSequence(ANTHONY, CAROLE, GABRIEL);
     }
-
-
 
 
 }
