af5c7c5b89948bd79a0d17bba47cb24feed40bd6
Arnaud Combellas <combellas.a@gmail.com>
2018-01-18 16:49:41 +0100

    Add mail object

---
+++ SocratesFrTest/CandidateManagement/Mail.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SocratesFrTest.CandidateManagement
+{
+    public class Mail
+    {
+        private string value;
+
+        public Mail(string value)
+        {
+            this.value = value;
+        }
+
+        public string GetValue()
+        {
+            return value;
+        }
+    }
+}
---
+++ SocratesFrTest/CandidateManagement/MailTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SocratesFr;
+using NUnit.Framework;
+using NFluent;
+
+namespace SocratesFrTest.CandidateManagement
+{
+    public class MailTest
+    {
+        [Test]
+        public void Equals_Same_Mail_Should_Return_True()
+        {
+            Mail mail = new Mail("toto@gmail.com");
+            Check.That(mail.GetValue().Equals("toto@gmail.com")).IsTrue();
+        }
+
+    }
+}
