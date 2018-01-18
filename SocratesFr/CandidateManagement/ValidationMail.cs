75976a23eaa81ce6bcbaa00096b361972880ce2c
Unknown <ebstein.bruno@gmail.com>
2018-01-18 16:38:28 +0100

    Add: should not validate mail if empty

---
+++ SocratesFr/CandidateManagement/ValidationMail.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SocratesFr.CandidateManagement
+{
+    public class ValidationMail
+    {
+        public static bool Validate(string v)
+        {
+            return false;
+        }
+    }
+}
---
+++ SocratesFrTest/CandidateManagement/ValidationMailShould.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NFluent;
+using NUnit.Framework;
+using SocratesFr.CandidateManagement;
+
+namespace SocratesFrTest.CandidateManagement
+{
+    class ValidationMailShould
+    {
+        [Test]
+        public void FailWhenMailIsEmpty()
+        {
+            Check.That(ValidationMail.Validate("")).IsFalse();
+        }
+    }
+}
