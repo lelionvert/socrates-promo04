343b8b5ae3f3fa9f0ddf5cea5210b9ae55db9e30
Arnaud Combellas <combellas.a@gmail.com>
2018-01-19 16:50:53 +0100

    Add candidate builder

---
+++ SocratesFr/CandidateManagement/CandidateBuilder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SocratesFr.CandidateManagement
+{
+    public class CandidateBuilder
+    {
+        public static void Create(string name, string email)
+        {
+            throw new ArgumentException();
+        }
+    }
+}
---
+++ SocratesFrTest/CandidateManagement/CandidateBuilderTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NFluent;
+using NUnit.Framework;
+using SocratesFr.CandidateManagement;
+
+namespace SocratesFrTest.CandidateManagement
+{
+    public class CandidateBuilderTest
+    {
+        [Test]
+        public void Invalid_Candidate_Should_Return_Error_Message()
+        {
+            Check.ThatCode(() => CandidateBuilder.Create("toto", "toto@gmail")).Throws<ArgumentException>();
+        }
+    }
+}
