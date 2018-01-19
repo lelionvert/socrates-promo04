6c555db509d2cdc9437ca60d2cd0f834eaf1bcc0
Arnaud Combellas <combellas.a@gmail.com>
2018-01-19 11:14:32 +0100

    create email builder

---
+++ SocratesFr/CandidateManagement/Email.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace SocratesFr.CandidateManagement
+{
+    public class Email
+    {
+        private string Address { get; }
+
+        private Email(string address)
+        {
+            this.Address = address;
+        }
+
+        public static Email EmailBuilder(string address)
+        {
+            Email email = new Email(address);
+            if (email.IsValidMail())
+                return email;
+            throw new ArgumentException("Invalid email address.");
+        }
+
+        public override bool Equals(object obj)
+        {
+            Email email = obj as Email;
+            return this != null &&
+                   this.Address == email.Address;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public bool IsValidMail()
+        {
+            string format = "^\\w+([-+.']\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*$";
+            return Regex.IsMatch(Address, format);
+        }
+    }
+}
---
+++ SocratesFrTest/CandidateManagement/EmailTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SocratesFr;
+using NUnit.Framework;
+using NFluent;
+using SocratesFr.CandidateManagement;
+
+namespace SocratesFrTest.CandidateManagement
+{
+    public class EmailTest
+    {
+        [Test]
+        public void Mails_Should_Be_Equals()
+        {
+            Email email = Email.EmailBuilder("koala@gmail.com");
+            Email mail2 = Email.EmailBuilder("koala@gmail.com");
+            Check.That(email.Equals(mail2)).IsTrue();
+        }
+
+        [TestCase("Koala@gmail.com")]
+        [TestCase("KoalaBamboo@gmail.com")]
+        public void IsInvalidEmail_Should_Allowed_Invalid_Email(string stringMail)
+        {
+            Email allowedEmail = Email.EmailBuilder(stringMail);
+            Check.That(allowedEmail.IsValidMail()).IsTrue();
+        }
+
+        [TestCase("Koala")]
+        [TestCase("@")]
+        [TestCase("@.fr")]
+        public void IsInvalidEmail_Should_Refused_Invalid_Email(string stringMail)
+        {
+            Check.ThatCode(() => Email.EmailBuilder(stringMail)).Throws<ArgumentException>();
+
+        }
+
+    }
+}
