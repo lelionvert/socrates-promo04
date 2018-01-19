36333f03a8ecff67634f9608eba4f005618eed16
brunoebstein <ebstein.bruno@gmail.com>
2018-01-19 16:54:39 +0100

    Rename tests and delete useless tests

--- SocratesFrTest/CandidateManagement/CandidateTest.cs
+++ SocratesFrTest/CandidateManagement/CandidateTest.cs
@@ -7,22 +7,7 @@ namespace SocratesFrTest.CandidateManagement
     public class CandidateTest
     {
         [Test]
-        public void Equals_Should_Return_False_When_Different_Candidates()
-        {
-            var candidateOne = new Candidate("Toto", Email.From("toto@gmail.com"));
-            var candidateTwo = new Candidate("Titi", Email.From("toto@gmail.com"));
-            Check.That(candidateOne.Equals(candidateTwo)).IsFalse();
-        }
-
-        [Test]
-        public void Equals_Should_Return_True_When_Two_Same_Candidates()
-        {
-            var candidateOne = new Candidate("Toto", Email.From("toto@gmail.com"));
-            Check.That(candidateOne.Equals(new Candidate("Toto", Email.From("toto@gmail.com")))).IsTrue();
-        }
-
-        [Test]
-        public void HasSameEmail_Should_Return_True_When_Email_Are_Same()
+        public void HasSameEmail_Two_Candidates_Should_Be_The_Same_When_They_Have_The_Same_Email()
         {
             var candidateOne = new Candidate("Toto", Email.From("toto@gmail.com"));
             var candidateTwo = new Candidate("Titi", Email.From("toto@gmail.com"));
@@ -30,27 +15,11 @@ namespace SocratesFrTest.CandidateManagement
         }
 
         [Test]
-        public void HasSameEmail_Should_Return_False_When_Email_Are_Different()
+        public void HasSameEmail_Two_Candidates_Should_Be_Different_When_They_Have_Not_The_Same_Email()
         {
             var candidateOne = new Candidate("Toto", Email.From("toto@gmail.com"));
             var candidateTwo = new Candidate("Titi", Email.From("titi@gmail.com"));
             Check.That(candidateOne.HasSameEmail(candidateTwo)).IsFalse();
-        }
-
-        [Test]
-        public void GetHashCode_Should_Be_Equal()
-        {
-            var candidateOne = new Candidate("Toto", Email.From("toto@gmail.com"));
-            var candidateTwo = new Candidate("Toto", Email.From("toto@gmail.com"));
-            Check.That(candidateOne.GetHashCode()).IsEqualTo(candidateTwo.GetHashCode());
-        }
-
-        [Test]
-        public void GetHashCode_Should_Not_Be_Equal()
-        {
-            var candidateOne = new Candidate("Toto", Email.From("toto@gmail.com"));
-            var candidateTwo = new Candidate("Titi", Email.From("toto@gmail.com"));
-            Check.That(candidateOne.GetHashCode()).IsNotEqualTo(candidateTwo.GetHashCode());
         }
     }
 }
