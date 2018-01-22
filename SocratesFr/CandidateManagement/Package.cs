f90cab9a48ad82dfc6221adb9f8ade9baeae883b
Arnaud Combellas <combellas.a@gmail.com>
2018-01-22 17:11:48 +0100

    Add Get price for single room full package

---
+++ SocratesFr/CandidateManagement/Package.cs
@@ -0,0 +1,24 @@
+﻿namespace SocratesFr.CandidateManagement
+{
+    public class Package
+    {
+        private Room room;
+        private int mealsNumber;
+
+        public enum Room
+        {
+            SINGLE = 0,
+            DOUBLE,
+            TRIPLE
+        }
+
+        public Package(Room roomType, int mealsNumber)
+        {
+            this.room = roomType;
+            this.mealsNumber = mealsNumber;
+            Price = 610;
+        }
+
+        public object Price { get; private set; }
+    }
+}
---
+++ SocratesFrTest/CandidateManagement/PackageTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using NFluent;
+using SocratesFr.CandidateManagement;
+
+namespace SocratesFrTest.CandidateManagement
+{
+    public class PackageTests
+    {
+        [Test]
+        public void Get_Price_For_Single_Room_Full_Package()
+        {
+            var package = new Package(Package.Room.SINGLE, 6);
+            Check.That(package.Price).Equals(610);
+        }
+    }
+}
