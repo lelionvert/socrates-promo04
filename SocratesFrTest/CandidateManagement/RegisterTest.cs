da225c71a7c8a2e5212578a8d101df2f10e2e31e
Arnaud Combellas <combellas.a@gmail.com>
2018-01-22 15:42:35 +0100

    Create test Register method and create test list method

--- SocratesFrTest/CandidateManagement/RegisterTest.cs
+++ SocratesFrTest/CandidateManagement/RegisterTest.cs
@@ -4,6 +4,7 @@ using SocratesFr.CandidateManagement;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using NUnit.Framework.Constraints;
 
 namespace SocratesFrTest.CandidateManagement
 {
@@ -43,15 +44,18 @@ namespace SocratesFrTest.CandidateManagement
         [Test]
         public void List_Of_Candidate_Should_Return_Sorted_List()
         {
-            Register register = new Register();
-            Candidate candidateOne = new Candidate("Toto", Email.Builder("toto@gmail.com"));
-            Candidate candidateTwo = new Candidate("Titi", Email.Builder("titi@gmail.com"));
-            Candidate candidateThree = new Candidate("Tati", Email.Builder("tati@gmail.com"));
-            register.AddCandidate(candidateOne);
-            register.AddCandidate(candidateTwo);
-            register.AddCandidate(candidateThree);
-            List<Candidate> listCandidates = register.GetCandidates();
-            Check.That(listCandidates).ContainsExactly(candidateThree, candidateTwo, candidateOne);        
+            Register register = CreateTestRegister(
+                ("Toto", "toto@socrates.com"),
+                ("Titi", "titi@socrates.com"),
+                ("Tata", "tata@socrates.com"));
+
+            List<Candidate> candidates = register.GetCandidates();
+
+            List<Candidate> orderedCandidatesList = CreateTestList(
+                ("Tata", "tata@socrates.com"),
+                ("Titi", "titi@socrates.com"),
+                ("Toto", "toto@socrates.com"));
+            Check.That(candidates).Equals(orderedCandidatesList);
         }
 
         [Test]
@@ -60,14 +64,14 @@ namespace SocratesFrTest.CandidateManagement
             Register register = new Register();
             string userEmail = "regis.dubois@socrates.com";
             string userName = "regis";
-
             register.AddCandidate(userName, userEmail);
 
             List<Candidate> listCandidates = register.GetCandidates();
+
             Check.That(listCandidates).ContainsExactly(CandidateBuilder.Create(userName, userEmail));
         }
 
-        private Register CreateTestRegister((string, string)[] tuples)
+        private Register CreateTestRegister(params (string, string)[] tuples)
         {
             Register register = new Register();
             foreach (var tuple in tuples)
@@ -75,6 +79,16 @@ namespace SocratesFrTest.CandidateManagement
                 register.AddCandidate(CandidateBuilder.Create(tuple.Item1, tuple.Item2));
             }
             return (register);
+        }
+
+        private List<Candidate> CreateTestList(params (string, string)[] tuples)
+        {
+            List<Candidate> candidates = new List<Candidate>();
+            foreach (var tuple in tuples)
+            {
+                candidates.Add(CandidateBuilder.Create(tuple.Item1, tuple.Item2));
+            }
+            return (candidates);
         }
     }
 }
