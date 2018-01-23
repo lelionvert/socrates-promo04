9d869ee190def7a5229387d9f7d1399387d23bcd
brunoebstein <ebstein.bruno@gmail.com>
2018-01-23 10:16:19 +0100

    Refactor price for single room without 1 meal

--- src/main/java/PriceCalculator.java
+++ src/main/java/PriceCalculator.java
@@ -2,13 +2,15 @@
  * Created by lenovo_3 on 22/01/2018.
  */
 public class PriceCalculator {
+    private static final int mealPrice = 40;
+
     public static int calculatePrice(Accommodation accommodation) {
         return calculatePrice(accommodation, 0);
     }
 
     public static int calculatePrice(Accommodation accommodation, int mealsNotTaken) {
         if (mealsNotTaken == 1)
-            return accommodation.price() - 40;
+            return accommodation.price() - mealPrice;
         return accommodation.price();
     }
 }
