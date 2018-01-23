2d15add25685adad7ca0965816856f5c29932b52
alexandre pouget <pouget.al@gmail.com>
2018-01-23 11:11:55 +0100

    add meals calculator

---
+++ src/main/java/MealsCalculator.java
@@ -0,0 +1,10 @@
+import java.util.Calendar;
+
+/**
+ * Created by lenovo_3 on 23/01/2018.
+ */
+public class MealsCalculator {
+    public static int calculate(Calendar c, Calendar c2) {
+        return 6;
+    }
+}
