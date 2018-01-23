c9776e1f8f7ec2c10b7ebf1904c87c71021787ae
alexandre pouget <pouget.al@gmail.com>
2018-01-23 11:15:41 +0100

    refactor meals calculator test

--- test/main/java/MealsCalculatorShould.java
+++ test/main/java/MealsCalculatorShould.java
@@ -1,13 +1,10 @@
 import org.assertj.core.api.Assertions;
-import org.junit.Before;
 import org.junit.Test;
-import sun.java2d.pipe.SpanShapeRenderer;
 
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
-import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.*;
 
 /**
  * Created by lenovo_3 on 23/01/2018.
@@ -16,14 +13,13 @@ public class MealsCalculatorShould {
 
     @Test
     public void giveMealNumberWhenCheckinThursdayAndCheckoutSundayAfter14PM() {
-        Calendar c = Calendar.getInstance();
-        c.add(Calendar.THURSDAY,1);
-        c.add(Calendar.HOUR,14);
-        Calendar c2 = Calendar.getInstance();
-        c2.setTime(new Date());
-        c2.add(Calendar.SUNDAY,1);
-        c2.add(Calendar.HOUR_OF_DAY,14);
-        int mealsNumber = MealsCalculator.calculate(c,c2);
-        Assertions.assertThat(mealsNumber).isEqualTo(6);
+        Calendar checkin = Calendar.getInstance();
+        checkin.add(Calendar.THURSDAY,1);
+        checkin.add(Calendar.HOUR_OF_DAY,14);
+        Calendar checkout = Calendar.getInstance();
+        checkout.add(Calendar.SUNDAY,1);
+        checkout.add(Calendar.HOUR_OF_DAY,14);
+        int mealsNumber = MealsCalculator.calculate(checkin,checkout);
+        assertThat(mealsNumber).isEqualTo(6);
     }
 }
