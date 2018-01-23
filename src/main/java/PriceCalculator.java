ddf25eb5aecd31c3ac81667a15638eebcff70b9d
brunoebstein <ebstein.bruno@gmail.com>
2018-01-23 15:16:00 +0100

    Refactor use convention style for constants

--- src/main/java/PriceCalculator.java
+++ src/main/java/PriceCalculator.java
@@ -4,21 +4,21 @@ import main.java.MinimumMealException;
  * Created by lenovo_3 on 22/01/2018.
  */
 public class PriceCalculator {
-    private static final double mealPrice = 40;
-    private static final int maximumMealsNotTaken = 2;
+    private static final double MEAL_PRICE = 40;
+    private static final int MAXIMUM_MEALS_NOT_TAKEN = 2;
 
     public static double calculatePrice(Accommodation accommodation) {
         return calculatePrice(accommodation, 0);
     }
 
     public static double calculatePrice(Accommodation accommodation, int mealsNotTaken) {
-        if (mealsNotTaken > maximumMealsNotTaken){
+        if (mealsNotTaken > MAXIMUM_MEALS_NOT_TAKEN){
             throw new MinimumMealException();
         }
         if (mealsNotTaken < 0){
             throw new IllegalArgumentException();
         }
 
-        return accommodation.price() - mealsNotTaken * mealPrice;
+        return accommodation.price() - mealsNotTaken * MEAL_PRICE;
     }
 }
