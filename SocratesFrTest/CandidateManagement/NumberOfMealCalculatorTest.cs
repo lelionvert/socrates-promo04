bcdae906b431959a78116fb36d43d3da76adf409
Arnaud Combellas <combellas.a@gmail.com>
2018-01-24 10:13:17 +0100

    Refacto test NumberOfMealsCalculator with new design

--- SocratesFrTest/CandidateManagement/NumberOfMealCalculatorTest.cs
+++ SocratesFrTest/CandidateManagement/NumberOfMealCalculatorTest.cs
@@ -13,16 +13,19 @@ namespace SocratesFrTest.CandidateManagement
         public const int FRIDAY = 26;
         public const int SATURDAY = 27;
         public const int SUNDAY = 28;
+        private int MEAL_MANDATORY = 4;
+        private int HOUR_OF_AFTERNOON_MEAL = 12;
+        private DayOfWeek DAY_BEGIN_SOCRATES = DayOfWeek.Thursday;
+        private DayOfWeek DAY_END_SOCRATES = DayOfWeek.Sunday;
 
         [TestCase(THURSDAY, 9, SATURDAY, 18)]
         [TestCase(FRIDAY, 9, SUNDAY, 18)]
         [TestCase(THURSDAY, 9, SUNDAY, 6)]
         public void Get_Number_Of_Meal_Taken_When_Missing_One_Meal(int dayArrival, int hourArrival, int dayDeparture, int hourDeparture)
         {                     
-            var mealCalculator = new NumberOfMealCalculator(CreateDateTimeOffset(dayArrival, hourArrival),
-                CreateDateTimeOffset(dayDeparture, hourDeparture));
+            var mealCalculator = new NumberOfMealCalculator(MEAL_MANDATORY, HOUR_OF_AFTERNOON_MEAL, DAY_BEGIN_SOCRATES, DAY_END_SOCRATES);
 
-            int numberOfMealTaken = mealCalculator.NumberOfMealTaken();
+            int numberOfMealTaken = mealCalculator.NumberOfMealTaken(CreateDateTimeOffset(dayArrival, hourArrival), CreateDateTimeOffset(dayDeparture, hourDeparture));
 
             Check.That(numberOfMealTaken).IsEqualTo(5);
         }
@@ -30,10 +33,9 @@ namespace SocratesFrTest.CandidateManagement
         [TestCase(THURSDAY, 9, SUNDAY, 18)]
         public void Get_Number_Of_Meal_Taken_When_Taking_All_Meals(int dayArrival, int hourArrival, int dayDeparture, int hourDeparture)
         {
-            var mealCalculator = new NumberOfMealCalculator(CreateDateTimeOffset(dayArrival, hourArrival),
-                CreateDateTimeOffset(dayDeparture, hourDeparture));
+            var mealCalculator = new NumberOfMealCalculator(MEAL_MANDATORY, HOUR_OF_AFTERNOON_MEAL, DAY_BEGIN_SOCRATES, DAY_END_SOCRATES);
 
-            int numberOfMealTaken = mealCalculator.NumberOfMealTaken();
+            int numberOfMealTaken = mealCalculator.NumberOfMealTaken(CreateDateTimeOffset(dayArrival, hourArrival), CreateDateTimeOffset(dayDeparture, hourDeparture));
 
             Check.That(numberOfMealTaken).IsEqualTo(6);
         }
@@ -42,10 +44,9 @@ namespace SocratesFrTest.CandidateManagement
         [TestCase(FRIDAY, 18, SATURDAY, 18)]
         public void Get_Number_Of_Meal_Taken_When_Missing_Two_Meals(int dayArrival, int hourArrival, int dayDeparture, int hourDeparture)
         {
-            var mealCalculator = new NumberOfMealCalculator(CreateDateTimeOffset(dayArrival, hourArrival),
-                CreateDateTimeOffset(dayDeparture, hourDeparture));
+            var mealCalculator = new NumberOfMealCalculator(MEAL_MANDATORY, HOUR_OF_AFTERNOON_MEAL, DAY_BEGIN_SOCRATES, DAY_END_SOCRATES);
 
-            int numberOfMealTaken = mealCalculator.NumberOfMealTaken();
+            int numberOfMealTaken = mealCalculator.NumberOfMealTaken(CreateDateTimeOffset(dayArrival, hourArrival), CreateDateTimeOffset(dayDeparture, hourDeparture));
 
             Check.That(numberOfMealTaken).IsEqualTo(4);
         }
