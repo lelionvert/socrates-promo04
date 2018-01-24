1307152bf0b70253e4d724b5dcf872634b0389ff
Arnaud Combellas <combellas.a@gmail.com>
2018-01-24 09:38:36 +0100

    Renaming arrivalDate and departureDate to checkin checkout

--- SocratesFr/CandidateManagement/NumberOfMealCalculator.cs
+++ SocratesFr/CandidateManagement/NumberOfMealCalculator.cs
@@ -4,26 +4,26 @@ namespace SocratesFr.CandidateManagement
 {
     public class NumberOfMealCalculator
     {
-        private DateTimeOffset arrivalDate;
-        private DateTimeOffset departureDate;
+        private DateTimeOffset checkin;
+        private DateTimeOffset checkout;
 
         private const int MEAL_MANDATORY = 4;
         private const int HOUR_OF_AFTERNOON_MEAL = 12;
         private DayOfWeek DAY_BEGIN_SOCRATES = DayOfWeek.Thursday;
         private DayOfWeek DAY_END_SOCRATES = DayOfWeek.Sunday;
 
-        public NumberOfMealCalculator(DateTimeOffset arrivalDate, DateTimeOffset departureDate)
+        public NumberOfMealCalculator(DateTimeOffset checkin, DateTimeOffset checkout)
         {
-            this.arrivalDate = arrivalDate;
-            this.departureDate = departureDate;
+            this.checkin = checkin;
+            this.checkout = checkout;
         }
 
         public int NumberOfMealTaken()
         {
             int mealTaken = MEAL_MANDATORY;
-            if (arrivalDate.DayOfWeek == DAY_BEGIN_SOCRATES)
+            if (checkin.DayOfWeek == DAY_BEGIN_SOCRATES)
                 mealTaken += 1;
-            if (departureDate.DayOfWeek == DAY_END_SOCRATES && departureDate.Hour > HOUR_OF_AFTERNOON_MEAL)
+            if (checkout.DayOfWeek == DAY_END_SOCRATES && checkout.Hour > HOUR_OF_AFTERNOON_MEAL)
                 mealTaken += 1;
 
             return mealTaken;
--- SocratesFr/CandidateManagement/PriceCalculator.cs
+++ SocratesFr/CandidateManagement/PriceCalculator.cs
@@ -15,35 +15,40 @@ namespace SocratesFr.CandidateManagement{
             NO_ACCOMODATION
         }
 
-        public PriceCalculator(Accommodation accommodation, DateTimeOffset arrivalDate, DateTimeOffset departureDate)
+        private const int ACCOMMODATION_SINGLE_PRICE = 370;
+        private const int ACCOMMODATION_DOUBLE_PRICE = 270;
+        private const int ACCOMMODATION_TRIPLE_PRICE = 170;
+        private const int NO_ACCOMMODATION_PRICE = 0;
+
+        public PriceCalculator(Accommodation accommodation, DateTimeOffset checkin, DateTimeOffset checkout)
         {
             this.accomodation = accommodation;
-            this.mealCalculator = new NumberOfMealCalculator(arrivalDate, departureDate);
+            this.mealCalculator = new NumberOfMealCalculator(checkin, checkout);
             int nbMealsTaken = mealCalculator.NumberOfMealTaken();
             switch (accomodation)
             {
                 case Accommodation.SINGLE:
-                    Price = CalculatePriceWithoutNMeal(370, nbMealsTaken);
+                    Price = CalculatePriceWithNMeal(ACCOMMODATION_SINGLE_PRICE, nbMealsTaken);
                     break;
                 case Accommodation.DOUBLE:
-                    Price = CalculatePriceWithoutNMeal(270, nbMealsTaken);
+                    Price = CalculatePriceWithNMeal(ACCOMMODATION_DOUBLE_PRICE, nbMealsTaken);
                     break;
                 case Accommodation.TRIPLE:
-                    Price = CalculatePriceWithoutNMeal(170, nbMealsTaken);
+                    Price = CalculatePriceWithNMeal(ACCOMMODATION_TRIPLE_PRICE, nbMealsTaken);
                     break;
                 case Accommodation.NO_ACCOMODATION:
-                    Price = CalculatePriceWithoutNMeal(0, nbMealsTaken);
+                    Price = CalculatePriceWithNMeal(NO_ACCOMMODATION_PRICE, nbMealsTaken);
                     break;
                 default:
                     throw new InvalidEnumArgumentException("Please select one of the four room price.");
             }
         }
 
-        private int CalculatePriceWithoutNMeal(int roomPrice, int nMeal)
+        private int CalculatePriceWithNMeal(int roomPrice, int nMeal)
         {
             return roomPrice + (nMeal * 40);
         }
 
-        public object Price { get; private set; }
+        public double Price { get; private set; }
     }
 }
