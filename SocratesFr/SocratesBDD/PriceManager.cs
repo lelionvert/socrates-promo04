9469b59df1f46822d7a561714fb14779d4c8e70c
Arnaud Combellas <combellas.a@gmail.com>
2018-01-24 11:17:28 +0100

    add PriceManager

---
+++ SocratesFr/SocratesBDD/PriceManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SocratesFr.SocratesBDD
+{
+    public class PriceManager
+    {
+        private Dictionary<string, double> productPriceDictionnary = new Dictionary<string, double>()
+        {
+            {"SINGLE", 370 }
+        };
+        public double GetProductPrice(string productKey)
+        {
+            return 370;
+        }
+    }
+}
---
+++ SocratesFrTest/SocratesBDD/PriceManagerTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NFluent;
+using NUnit.Framework;
+using SocratesFr.SocratesBDD;
+
+namespace SocratesFrTest.SocratesBDD
+{
+    public class PriceManagerTest
+    {
+        [Test]
+        public void Get_Accomodation_Single_Price()
+        {
+            PriceManager priceManager = new PriceManager();
+
+            var price = priceManager.GetProductPrice("SINGLE");
+
+            Check.That(price).IsEqualTo(370);
+        }
+    }
+}
