ce3a01af4f277f56c036c01385ddaf466242e82d
alexandre pouget <pouget.al@gmail.com>
2018-01-24 16:13:34 +0100

    refactor MealsCalculator

--- src/main/java/MealsCalculator.java
+++ src/main/java/MealsCalculator.java
@@ -1,13 +1,6 @@
 import java.time.LocalDateTime;
-import java.util.Calendar;
 
-/**
- * Created by lenovo_3 on 23/01/2018.
- */
 public class MealsCalculator {
-    private static final int DINNER_HOUR = 21;
-    private static final int LUNCH_HOUR = 12;
-    public static final int MAX_MEALS_NUMBER = 6;
     private final LocalDateTime firstMeal;
     private final LocalDateTime lastMeal;
     private final int mandatoryMeals;
@@ -18,19 +11,10 @@ public class MealsCalculator {
         this.mandatoryMeals = mandatoryMeals;
     }
 
-    public static int calculate(Calendar checkin, Calendar checkout) {
-        int mealsNumber = MAX_MEALS_NUMBER;
-        if(checkin.get(Calendar.HOUR_OF_DAY) > DINNER_HOUR || checkin.get(Calendar.AM_PM) == Calendar.AM){
-            mealsNumber--;
-        }
-        if(checkout.get(Calendar.HOUR_OF_DAY) < LUNCH_HOUR){
-            mealsNumber--;
-        }
+    public int calculate(CheckTime checkin, CheckTime checkout){
+        int mealsNumber = mandatoryMeals;
+        if(checkin.isBefore(firstMeal)) mealsNumber++;
+        if(!checkout.isBefore(lastMeal)) mealsNumber++;
         return mealsNumber;
-    }
-
-    public int calculate(CheckTime checkIn, CheckTime checkOut){
-        if(checkIn.isBefore(firstMeal)) return 6;
-        return 5;
     }
 }
--- test/main/java/MealsCalculatorShould.java
+++ test/main/java/MealsCalculatorShould.java
@@ -1,28 +1,11 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
-import java.util.Calendar;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-/**
- * Created by lenovo_3 on 23/01/2018.
- */
 public class MealsCalculatorShould {
-
-    private Calendar createCalendar(String date){
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy:h a");
-        Calendar calendar = Calendar.getInstance();
-        try {
-            calendar.setTime(sdf.parse(date));
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        return calendar;
-    }
 
     private MealsCalculator mealsCalculator;
 
@@ -35,49 +18,33 @@ public class MealsCalculatorShould {
 
     @Test
     public void giveMaxMealNumber() {
-        Calendar checkin = createCalendar("25/01/2018:2 PM");
-        Calendar checkout = createCalendar("28/01/2018:2 PM");
-        int mealsNumber = MealsCalculator.calculate(checkin,checkout);
-        assertThat(mealsNumber).isEqualTo(MealsCalculator.MAX_MEALS_NUMBER);
-    }
-
-    @Test
-    public void giveMaxMealNumber2() {
         CheckTime checkin = CheckTime.parse("2018-01-25T14:00:00");
         CheckTime checkout = CheckTime.parse("2018-01-28T14:00:00");
         int mealsNumber = mealsCalculator.calculate(checkin,checkout);
-        assertThat(mealsNumber).isEqualTo(MealsCalculator.MAX_MEALS_NUMBER);
+        assertThat(mealsNumber).isEqualTo(6);
     }
 
     @Test
     public void giveMealsNumberWithoutThursdayDinner(){
-        Calendar checkin = createCalendar("26/01/2018:10 AM");
-        Calendar checkout = createCalendar("28/01/2018:2 PM");
-        int mealsNumber = MealsCalculator.calculate(checkin,checkout);
-        assertThat(mealsNumber).isEqualTo(MealsCalculator.MAX_MEALS_NUMBER-1);
+        CheckTime checkin = CheckTime.parse("2018-01-25T14:00:00");
+        CheckTime checkout = CheckTime.parse("2018-01-28T10:00:00");
+        int mealsNumber = mealsCalculator.calculate(checkin,checkout);
+        assertThat(mealsNumber).isEqualTo(5);
     }
 
     @Test
     public void giveMealsNumberWithtoutThursdayDinnerAndSundayLunch(){
-        Calendar checkin = createCalendar("26/01/2018:10 AM");
-        Calendar checkout = createCalendar("28/01/2018:10 AM");
-        int mealsNumber = MealsCalculator.calculate(checkin,checkout);
-        assertThat(mealsNumber).isEqualTo(MealsCalculator.MAX_MEALS_NUMBER-2);
+        CheckTime checkin = CheckTime.parse("2018-01-26T10:00:00");
+        CheckTime checkout = CheckTime.parse("2018-01-28T10:00:00");
+        int mealsNumber = mealsCalculator.calculate(checkin,checkout);
+        assertThat(mealsNumber).isEqualTo(4);
     }
 
     @Test
     public void giveMealsNumberWhenCheckinAfterThursdayDinner(){
-        Calendar checkin = createCalendar("25/01/2018:10 PM");
-        Calendar checkout = createCalendar("28/01/2018:2 PM");
-        int mealsNumber = MealsCalculator.calculate(checkin,checkout);
-        assertThat(mealsNumber).isEqualTo(MealsCalculator.MAX_MEALS_NUMBER-1);
-    }
-
-    @Test
-    public void giveMealsNumberWhenCheckinAfterThursdayDinner2(){
         CheckTime checkin = CheckTime.parse("2018-01-25T22:00:00");
         CheckTime checkout = CheckTime.parse("2018-01-28T14:00:00");
         int mealsNumber = mealsCalculator.calculate(checkin,checkout);
-        assertThat(mealsNumber).isEqualTo(MealsCalculator.MAX_MEALS_NUMBER-1);
+        assertThat(mealsNumber).isEqualTo(5);
     }
 }
