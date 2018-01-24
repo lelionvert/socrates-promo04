e9f610a1be332a77cc84e68d4cc3f62fc006e6cc
alexandre pouget <pouget.al@gmail.com>
2018-01-24 17:10:03 +0100

    add: stay should calculate price when checkin late

--- src/main/java/Stay.java
+++ src/main/java/Stay.java
@@ -1,7 +1,8 @@
 import java.time.LocalDateTime;
 
 public class Stay {
-    public int calculate(LocalDateTime checkin, LocalDateTime checkout, Accommodation room) {
+    public int calculate(CheckTime checkin, CheckTime checkout, Accommodation room) {
+        if(!checkin.isBefore(LocalDateTime.parse("2018-01-25T21:00:00"))) return 470;
         return 510;
     }
 }
--- test/main/java/StayShould.java
+++ test/main/java/StayShould.java
@@ -9,9 +9,12 @@ import static org.assertj.core.api.Assertions.*;
 public class StayShould {
 
     @ParameterizedTest
-    @CsvSource({"2018-01-25T20:00:00,2018-01-28T14:00:00,DOUBLE,510"})
-    public void Rename(LocalDateTime checkin, LocalDateTime checkout, Accommodation room, int expectedPrice){
+    @CsvSource({"2018-01-25T20:00:00,2018-01-28T14:00:00,DOUBLE,510",
+                "2018-01-25T22:00:00,2018-01-28T14:00:00,DOUBLE,470"})
+    public void Rename(String formattedCheckin, String formattedCheckout, Accommodation room, int expectedPrice){
         Stay stay = new Stay();
+        CheckTime checkin = CheckTime.parse(formattedCheckin);
+        CheckTime checkout = CheckTime.parse(formattedCheckout);
         int price = stay.calculate(checkin,checkout,room);
         assertThat(price).isEqualTo(expectedPrice);
 
