c2f2978a5e38654c4b41fbee797e4a5e7fa8b0cb
alexandre pouget <pouget.al@gmail.com>
2018-01-24 15:08:49 +0100

    refactor stay test to parameterized test

--- src/main/java/Stay.java
+++ src/main/java/Stay.java
@@ -1,23 +1,17 @@
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Calendar;
 
 /**
  * Created by lenovo_3 on 23/01/2018.
  */
 public class Stay {
 
-    private LocalDateTime date;
+    private final LocalDateTime date;
 
     public Stay(LocalDateTime date) {
         this.date = date;
     }
 
     public boolean checkinBefore(LocalDateTime date) {
-        if(this.date.equals(date))return true;
-        LocalDateTime expectedDate = LocalDateTime.parse("2018-01-25T22:00:00", DateTimeFormatter.ISO_LOCAL_DATE_TIME);
-        if(date.compareTo(expectedDate) == 0) return false;
-        return true;
+        return !this.date.isAfter(date);
     }
 }
--- test/main/java/StayShould.java
+++ test/main/java/StayShould.java
@@ -1,16 +1,9 @@
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.stream.Stream;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class StayShould {
 
