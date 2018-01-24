3d1f9be017fc4fd097c42952795c8735378f9714
Arnaud Combellas <combellas.a@gmail.com>
2018-01-24 09:59:54 +0100

    Refacto test Price Calculator with new design

--- SocratesFr/CandidateManagement/PriceCalculator.cs
+++ SocratesFr/CandidateManagement/PriceCalculator.cs
@@ -4,7 +4,6 @@ using System.ComponentModel;
 namespace SocratesFr.CandidateManagement{
     public class PriceCalculator
     {
-        private Accommodation accomodation;
         private NumberOfMealCalculator mealCalculator;
 
         public enum Accommodation
@@ -29,36 +28,12 @@ namespace SocratesFr.CandidateManagement{
         {
             this.mealCalculator = new NumberOfMealCalculator(MEAL_MANDATORY, HOUR_OF_AFTERNOON_MEAL, DAY_BEGIN_SOCRATES, DAY_END_SOCRATES);
         }
-
-        public PriceCalculator(Accommodation accommodation, DateTimeOffset checkin, DateTimeOffset checkout)
-        {
-            this.accomodation = accommodation;
-            this.mealCalculator = new NumberOfMealCalculator(checkin, checkout);
-            int nbMealsTaken = mealCalculator.NumberOfMealTaken();
-            switch (accomodation)
-            {
-                case Accommodation.SINGLE:
-                    Price = CalculatePriceWithNMeal(ACCOMMODATION_SINGLE_PRICE, nbMealsTaken);
-                    break;
-                case Accommodation.DOUBLE:
-                    Price = CalculatePriceWithNMeal(ACCOMMODATION_DOUBLE_PRICE, nbMealsTaken);
-                    break;
-                case Accommodation.TRIPLE:
-                    Price = CalculatePriceWithNMeal(ACCOMMODATION_TRIPLE_PRICE, nbMealsTaken);
-                    break;
-                case Accommodation.NO_ACCOMODATION:
-                    Price = CalculatePriceWithNMeal(NO_ACCOMMODATION_PRICE, nbMealsTaken);
-                    break;
-                default:
-                    throw new InvalidEnumArgumentException("Please select one of the four room price.");
-            }
-        }
-
+      
         public int CalculatePrice(Accommodation accommodation, DateTimeOffset checkin, DateTimeOffset checkout)
         {
             int totalPrice = 0;
             int nbMealsTaken = mealCalculator.NumberOfMealTaken(checkin, checkout);
-            switch (accomodation)
+            switch (accommodation)
             {
                 case Accommodation.SINGLE:
                     totalPrice = CalculatePriceWithNMeal(ACCOMMODATION_SINGLE_PRICE, nbMealsTaken);
--- SocratesFrTest/CandidateManagement/PriceCalculatorTests.cs
+++ SocratesFrTest/CandidateManagement/PriceCalculatorTests.cs
@@ -21,56 +21,64 @@ namespace SocratesFrTest.CandidateManagement
         [Test]
         public void Get_Price_For_Double_Room()
         {
-            var package = new PriceCalculator(PriceCalculator.Accommodation.DOUBLE, CreateDateTimeOffset(25, 9), CreateDateTimeOffset(28, 18));
-            Check.That(package.Price).Equals(510);
+            var priceCalculator = new PriceCalculator();
+            int price = priceCalculator.CalculatePrice(PriceCalculator.Accommodation.DOUBLE, CreateDateTimeOffset(25, 9), CreateDateTimeOffset(28, 18));
+            Check.That(price).Equals(510);
         }
 
         [Test]
         public void Get_Price_For_Triple_Room()
         {
-            var package = new PriceCalculator(PriceCalculator.Accommodation.TRIPLE, CreateDateTimeOffset(25, 9), CreateDateTimeOffset(28, 18));
-            Check.That(package.Price).Equals(410);
+            var priceCalculator = new PriceCalculator();
+            int price = priceCalculator.CalculatePrice(PriceCalculator.Accommodation.TRIPLE, CreateDateTimeOffset(25, 9), CreateDateTimeOffset(28, 18));
+            Check.That(price).Equals(410);
         }
 
         [Test]
         public void Get_Price_For_No_Accomodation_Room()
         {
-            var package = new PriceCalculator(PriceCalculator.Accommodation.NO_ACCOMODATION, CreateDateTimeOffset(25, 9), CreateDateTimeOffset(28, 18));
-            Check.That(package.Price).Equals(240);
+            var priceCalculator = new PriceCalculator();
+            int price = priceCalculator.CalculatePrice(PriceCalculator.Accommodation.NO_ACCOMODATION, CreateDateTimeOffset(25, 9), CreateDateTimeOffset(28, 18));
+            Check.That(price).Equals(240);
         }
 
         [Test]
         public void Get_Price_For_Unknown_Selection()
-        { 
-            Check.ThatCode(() => new PriceCalculator((PriceCalculator.Accommodation)int.MaxValue, CreateDateTimeOffset(25, 9), CreateDateTimeOffset(28, 18))).Throws<InvalidEnumArgumentException>();
+        {
+            var priceCalculator = new PriceCalculator();            
+            Check.ThatCode(() => priceCalculator.CalculatePrice((PriceCalculator.Accommodation)int.MaxValue, CreateDateTimeOffset(25, 9), CreateDateTimeOffset(28, 18))).Throws<InvalidEnumArgumentException>();
         }
 
         [Test]
         public void Get_Price_For_Single_Room_Without_One_Meal_Checkin_Friday_Chekout_Sunday()
         {
-            var package = new PriceCalculator(PriceCalculator.Accommodation.SINGLE, CreateDateTimeOffset(26, 9), CreateDateTimeOffset(28, 18));
-            Check.That(package.Price).Equals(570);
+            var priceCalculator = new PriceCalculator();
+            int price = priceCalculator.CalculatePrice(PriceCalculator.Accommodation.SINGLE, CreateDateTimeOffset(26, 9), CreateDateTimeOffset(28, 18));
+            Check.That(price).Equals(570);
         }
 
         [Test]
         public void Get_Price_For_Double_Room_Without_One_Meal_Checkin_Friday_Chekout_Sunday()
         {
-            var package = new PriceCalculator(PriceCalculator.Accommodation.DOUBLE, CreateDateTimeOffset(26, 9), CreateDateTimeOffset(28, 18));
-            Check.That(package.Price).Equals(470);
+            var priceCalculator = new PriceCalculator();
+            int price = priceCalculator.CalculatePrice(PriceCalculator.Accommodation.DOUBLE, CreateDateTimeOffset(26, 9), CreateDateTimeOffset(28, 18));
+            Check.That(price).Equals(470);
         }
 
         [Test]
         public void Get_Price_For_Triple_Room_Without_One_Meal_Checkin_Friday_Chekout_Sunday()
         {
-            var package = new PriceCalculator(PriceCalculator.Accommodation.TRIPLE, CreateDateTimeOffset(26, 9), CreateDateTimeOffset(28, 18));
-            Check.That(package.Price).Equals(370);
+            var priceCalculator = new PriceCalculator();
+            int price = priceCalculator.CalculatePrice(PriceCalculator.Accommodation.TRIPLE, CreateDateTimeOffset(26, 9), CreateDateTimeOffset(28, 18));
+            Check.That(price).Equals(370);
         }
 
         [Test]
         public void Get_Price_For_No_Accomodation_Room_Without_One_Meal_Checkin_Friday_Chekout_Sunday()
         {
-            var package = new PriceCalculator(PriceCalculator.Accommodation.NO_ACCOMODATION, CreateDateTimeOffset(26, 9), CreateDateTimeOffset(28, 18));
-            Check.That(package.Price).Equals(200);
+            var priceCalculator = new PriceCalculator();
+            int price = priceCalculator.CalculatePrice(PriceCalculator.Accommodation.NO_ACCOMODATION, CreateDateTimeOffset(26, 9), CreateDateTimeOffset(28, 18));
+            Check.That(price).Equals(200);
         }
 
         private DateTimeOffset CreateDateTimeOffset(int day, int hour)
