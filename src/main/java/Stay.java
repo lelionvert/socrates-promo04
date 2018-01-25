002d2ba9c1d27403386a8f7b53caae3aa175871a
alexandre pouget <pouget.al@gmail.com>
2018-01-25 09:16:42 +0100

    add: stay should calculate price when checkin late and checkout before

--- src/main/java/Stay.java
+++ src/main/java/Stay.java
@@ -2,7 +2,8 @@ import java.time.LocalDateTime;
 
 public class Stay {
     public int calculate(CheckTime checkin, CheckTime checkout, Accommodation room) {
+        if(checkout.isBefore(LocalDateTime.parse("2018-01-28T12:00:00"))) return 430;
         if(!checkin.isBefore(LocalDateTime.parse("2018-01-25T21:00:00"))) return 470;
-        return 510;
+            return 510;
     }
 }
