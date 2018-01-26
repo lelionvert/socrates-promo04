a164a5ef6f50f1f8d5022c3b421e082884ff6184
Arnaud Combellas <combellas.a@gmail.com>
2018-01-26 17:33:12 +0100

    Calculate Cold Meal: refactor implementation to use kitchen

--- SocratesFrTest/ColdMealManagement/ColdMealCalculator.cs
+++ SocratesFrTest/ColdMealManagement/ColdMealCalculator.cs
@@ -1,19 +1,26 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using NUnit.Framework;
+using SocratesFr.ColdMealManagement;
 
 namespace SocratesFrTest.ColdMealManagement
 {
     public class ColdMealCalculator
     {
+        private readonly Kitchen kitchen;
+
+        public ColdMealCalculator(Kitchen kitchen)
+        {
+            this.kitchen = kitchen;
+        }
+        
         public int Calculate(IList<DateTime> checkIns )
         {
-            if (checkIns == null || checkIns.Count == 0)
+            if (checkIns == null)
                 return 0;
-            if (checkIns.Contains(new DateTime(2018, 1, 25, 21, 0, 0)))
-                return 2;
-            return checkIns.Count;
+            return checkIns.Count(checkIn => kitchen.HasColdMealAvailableAt(checkIn));
         }
     }
 }
--- SocratesFrTest/ColdMealManagement/ColdMealCalculatorTest.cs
+++ SocratesFrTest/ColdMealManagement/ColdMealCalculatorTest.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Text;
 using NFluent;
 using NUnit.Framework;
+using SocratesFr.ColdMealManagement;
 
 namespace SocratesFrTest.ColdMealManagement
 {
     public class ColdMealCalculatorTest
     {
+        private Kitchen kitchen = new Kitchen(new DateTime(2018, 1, 25, 21, 0, 0), new DateTime(2018, 1, 26, 0, 0, 0));
 
         private List<DateTime> CheckInsGenerator(params DateTime[] checkins)
         {
@@ -17,35 +19,35 @@ namespace SocratesFrTest.ColdMealManagement
         [Test]
         public void Calculate_With_Empty_List()
         {
-            ColdMealCalculator coldMealCalculator = new ColdMealCalculator();
+            ColdMealCalculator coldMealCalculator = new ColdMealCalculator(kitchen);
             Check.That(coldMealCalculator.Calculate(CheckInsGenerator())).IsEqualTo(0);
         }
 
         [Test]
         public void Calculate_With_One_Check_In_In_Range()
         {
-            ColdMealCalculator coldMealCalculator = new ColdMealCalculator();
+            ColdMealCalculator coldMealCalculator = new ColdMealCalculator(kitchen);
             Check.That(coldMealCalculator.Calculate(CheckInsGenerator(new DateTime(2018, 1, 25, 22, 0, 0)))).IsEqualTo(1);
         }
 
         [Test]
         public void Calculate_With_Null_Check_Ins()
         {
-            ColdMealCalculator coldMealCalculator = new ColdMealCalculator();
+            ColdMealCalculator coldMealCalculator = new ColdMealCalculator(kitchen);
             Check.That(coldMealCalculator.Calculate(null)).IsEqualTo(0);
         }
 
         [Test]
         public void Calculate_With_Two_Check_Ins_In_Range()
         {
-            ColdMealCalculator coldMealCalculator = new ColdMealCalculator();
+            ColdMealCalculator coldMealCalculator = new ColdMealCalculator(kitchen);
             Check.That(coldMealCalculator.Calculate(CheckInsGenerator(new DateTime(2018, 1, 25, 22, 0, 0), new DateTime(2018, 1, 26, 0, 0, 0)))).IsEqualTo(2);
         }
 
         [Test]
         public void Calculate_With_Two_Check_Ins_In_Range_And_One_Out_Of_Range()
         {
-            ColdMealCalculator coldMealCalculator = new ColdMealCalculator();
+            ColdMealCalculator coldMealCalculator = new ColdMealCalculator(kitchen);
             Check.That(coldMealCalculator.Calculate(CheckInsGenerator(new DateTime(2018, 1, 25, 22, 0, 0), new DateTime(2018, 1, 26, 0, 0, 0), new DateTime(2018, 1, 25, 21, 0, 0)))).IsEqualTo(2);
         }
     }
