ef3f383b3703849be5eca5185f409803ab98f6f6
Arnaud Combellas <combellas.a@gmail.com>
2018-01-26 15:37:37 +0100

    Add: tests cases for check in is in range

--- SocratesFrTest/ColdMealManagement/KitchenTest.cs
+++ SocratesFrTest/ColdMealManagement/KitchenTest.cs
@@ -9,17 +9,18 @@ namespace SocratesFrTest.ColdMealManagement
 {
     public class KitchenTest
     {
-
+        [TestCase(25, 21, 1)]
         [TestCase(25, 22, 0)]
+        [TestCase(26, 0, 0)]
         public void Cold_Meal_Available_When_Check_In_Is_In_Range(int day, int hour, int minute)
         {
             Check.That(new Kitchen().HasColdMealAvailableAt(new DateTime(2018, 1, day, hour, minute, 0))).IsTrue();
         }
 
-        [TestCase(26, 1, 0)]
-        [TestCase(25, 21, 0)]
         [TestCase(25, 19, 0)]
+        [TestCase(25, 21, 0)]
         [TestCase(26, 0, 1)]
+        [TestCase(26, 1, 0)]
         public void No_Cold_Meal_Available_When_Check_In_Is_Out_Of_Range(int day, int hour, int minute)
         {
             Check.That(new Kitchen().HasColdMealAvailableAt(new DateTime(2018, 1, day, hour, minute, 0))).IsFalse();
