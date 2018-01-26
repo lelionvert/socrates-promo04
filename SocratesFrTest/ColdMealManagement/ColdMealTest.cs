7143e04c8351cffe34b3d449462739ccf8e194c4
Arnaud Combellas <combellas.a@gmail.com>
2018-01-26 14:43:16 +0100

    Add test for check in at 7pm

--- SocratesFrTest/ColdMealManagement/ColdMealTest.cs
+++ SocratesFrTest/ColdMealManagement/ColdMealTest.cs
@@ -17,10 +17,11 @@ namespace SocratesFrTest.ColdMealManagement
             Check.That(isCold).IsTrue();
         }
 
-        [Test]
-        public void Meal_Is_Not_Cold_When_Check_In_At_1()
+        [TestCase(26,1,0)]
+        [TestCase(25,19,0)]
+        public void Meal_Is_Not_Cold_When_Check_In_At_1(int day, int hour, int minute)
         {
-            Check.That(new ColdMeal().IsCold(new DateTime(2018, 1, 26, 1, 0, 0))).IsFalse();
+            Check.That(new ColdMeal().IsCold(new DateTime(2018, 1, day, hour, minute, 0))).IsFalse();
         }
     }
 }
