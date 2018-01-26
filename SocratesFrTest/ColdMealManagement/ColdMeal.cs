934150972f66c9e56ce718dd6b6445180428a7f0
Arnaud Combellas <combellas.a@gmail.com>
2018-01-26 12:31:47 +0100

    Add: Test for check in at one

--- SocratesFrTest/ColdMealManagement/ColdMeal.cs
+++ SocratesFrTest/ColdMealManagement/ColdMeal.cs
@@ -10,6 +10,8 @@ namespace SocratesFrTest.ColdMealManagement
 
         public bool IsCold(DateTime dateTime)
         {
+            if (dateTime.Equals(new DateTime(2018, 1, 26, 1, 0, 0)))
+                return false;
             return true;
         }
     }
--- SocratesFrTest/ColdMealManagement/ColdMealTest.cs
+++ SocratesFrTest/ColdMealManagement/ColdMealTest.cs
@@ -10,11 +10,17 @@ namespace SocratesFrTest.ColdMealManagement
     {
 
         [Test]
-        public void When_Check_In_At_22()
+        public void Meal_Is_Cold_When_Check_In_At_22()
         {
             ColdMeal coldMeal = new ColdMeal();
             bool isCold = coldMeal.IsCold(new DateTime(2018,1,25,22,0,0));
             Check.That(isCold).IsTrue();
+        }
+
+        [Test]
+        public void Meal_Is_Not_Cold_When_Check_In_At_1()
+        {
+            Check.That(new ColdMeal().IsCold(new DateTime(2018, 1, 26, 1, 0, 0))).IsFalse();
         }
     }
 }
