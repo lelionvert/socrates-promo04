57a83d6f669a156ea6f6e728cd3571093d0fe8d7
Arnaud Combellas <combellas.a@gmail.com>
2018-01-26 15:43:27 +0100

    Add: constructor with parameters

--- SocratesFr/ColdMealManagement/Kitchen.cs
+++ SocratesFr/ColdMealManagement/Kitchen.cs
@@ -4,15 +4,25 @@ namespace SocratesFr.ColdMealManagement
 {
     public class Kitchen
     {
-        public Kitchen()
+        private readonly DateTime _coldMealBegin;
+        private readonly DateTime _coldMealEnd;
+
+        public Kitchen() : this(new DateTime(2018, 1, 25, 21, 0, 0), new DateTime(2018, 1, 26, 0, 0, 0)) 
         {
+
+        }
+
+        public Kitchen(DateTime coldMealBegin, DateTime coldMealEnd)
+        {
+            _coldMealBegin = coldMealBegin;
+            _coldMealEnd = coldMealEnd;
         }
 
         public bool HasColdMealAvailableAt(DateTime dateTime)
         {
-            if (dateTime <= (new DateTime(2018, 1, 25, 21, 0, 0)))
+            if (dateTime <= _coldMealBegin)
                 return false;
-            if (dateTime >(new DateTime(2018, 1, 26, 0, 0, 0)))
+            if (dateTime > _coldMealEnd)
                 return false;
             return true;
         }
