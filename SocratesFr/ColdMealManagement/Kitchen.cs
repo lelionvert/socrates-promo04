191887f9a3430aada058f28e9d3b3278071e524c
Arnaud Combellas <combellas.a@gmail.com>
2018-01-26 15:07:09 +0100

    Rename: ColdMeal To kitchen

---
+++ SocratesFr/ColdMealManagement/Kitchen.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SocratesFr.ColdMealManagement
+{
+    public class Kitchen
+    {
+        public Kitchen()
+        {
+        }
+
+        public bool HasColdMealAvailableAt(DateTime dateTime)
+        {
+            if (dateTime.Equals(new DateTime(2018, 1, 25, 19, 0, 0)))
+                return false;
+            if (dateTime.Equals(new DateTime(2018, 1, 26, 1, 0, 0)))
+                return false;
+            return true;
+        }
+    }
+}
--- SocratesFrTest/ColdMealManagement/KitchenTest.cs
+++ SocratesFrTest/ColdMealManagement/KitchenTest.cs
@@ -7,22 +7,22 @@ using SocratesFr.ColdMealManagement;
 
 namespace SocratesFrTest.ColdMealManagement
 {
-    public class ColdMealTest
+    public class KitchenTest
     {
 
         [Test]
         public void Meal_Is_Cold_When_Check_In_At_22()
         {
-            ColdMeal coldMeal = new ColdMeal();
-            bool isCold = coldMeal.IsCold(new DateTime(2018,1,25,22,0,0));
+            Kitchen kitchen = new Kitchen();
+            bool isCold = kitchen.HasColdMealAvailableAt(new DateTime(2018, 1, 25, 22, 0, 0));
             Check.That(isCold).IsTrue();
         }
 
-        [TestCase(26,1,0)]
-        [TestCase(25,19,0)]
+        [TestCase(26, 1, 0)]
+        [TestCase(25, 19, 0)]
         public void Meal_Is_Not_Cold_When_Check_In_At_1(int day, int hour, int minute)
         {
-            Check.That(new ColdMeal().IsCold(new DateTime(2018, 1, day, hour, minute, 0))).IsFalse();
+            Check.That(new Kitchen().HasColdMealAvailableAt(new DateTime(2018, 1, day, hour, minute, 0))).IsFalse();
         }
     }
 }
