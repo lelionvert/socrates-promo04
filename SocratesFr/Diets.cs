26847301912efcddfbfc903ee79c14214fe05e5c
Anthony <anthony.nizan@gmail.com>
2018-01-29 16:05:03 +0100

    Add: Test Diets and CountVegetarian

---
+++ SocratesFr/Diets.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace SocratesFr
+{
+    public class Diets
+    {
+        private readonly List<Diet> diets;
+
+        public int CountVegetarian()
+        {
+            return diets.Count(c => c.Equals(Diet.VEGETARIAN));
+        }
+
+        public Diets()
+        {
+            this.diets = new List<Diet>();
+        }
+    }
+
+    public enum Diet
+    {
+        VEGETARIAN
+    }
+}
---
+++ SocratesFrTest/DietCalculatorTest.cs
@@ -0,0 +1,25 @@
+﻿using NFluent;
+using NUnit.Framework;
+using SocratesFr;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SocratesFr;
+
+namespace SocratesFrTest
+{
+    public class DietCalculatorTest
+    {
+        [Test]
+        public void When_No_Vegetarian_Participant_Return_0()
+        {
+            Diets diets = new Diets();
+            Check.That(diets.CountVegetarian()).IsEqualTo(0);
+        }
+    }
+
+    
+        
+}
