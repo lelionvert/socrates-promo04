2092ea342b8a5369e9f3b8732ca62bf83192c488
Anthony <anthony.nizan@gmail.com>
2018-01-30 10:30:24 +0100

    Add: Meals and MealsTest with test without diet

---
+++ SocratesFrTest/Meals.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using SocratesFr;
+
+namespace SocratesFrTest
+{
+    internal class Meals
+    {
+        public int Count(DateTime mealTime, Diet diet)
+        {
+            return 0;
+        }
+    }
+}
---
+++ SocratesFrTest/MealsTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NFluent;
+using NUnit.Framework;
+using SocratesFr;
+
+namespace SocratesFrTest
+{
+    class MealsTest
+    {
+        [Test]
+        public void No_Diets_For_One_Date_Should_Not_Gives_Covers()
+        {
+            Meals meals = new Meals();
+            int count = meals.Count(new DateTime(2018, 1, 25, 12, 0, 0), Diet.VEGAN);
+            Check.That(count).IsEqualTo(0);
+        }
+    }
+}
