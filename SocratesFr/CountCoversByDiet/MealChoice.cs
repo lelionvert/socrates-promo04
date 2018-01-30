d377b987e941873b9fbde3cfeb8f5ee3e8a0c22a
brunoebstein <ebstein.bruno@gmail.com>
2018-01-30 14:10:21 +0100

    Refactor: delete useless tests and methods

--- SocratesFr/CountCoversByDiet/MealChoice.cs
+++ SocratesFr/CountCoversByDiet/MealChoice.cs
@@ -7,35 +7,10 @@ namespace SocratesFr.CountCoversByDiet
         public MealChoice()
         {
         }
-
-        public int GetVegetarianMeal()
-        {
-            return 1;
-        }
-
-        public int GetVeganMeal()
-        {
-            return 1;
-        }
-
-        public int GetPescatarianMeal()
-        {
-            return 1;
-        }
-
-        public int GetVegetarianMeal(MealTime fridayLunch)
-        {
-            return 1;
-        }
-
-        public int GetVeganMeal(MealTime sundayLunch)
-        {
-            return 1;
-        }
-
+        
         public int CountCovers(MealTime mealTime, Diet diet)
         {
-            return GetVeganMeal();
+            return 1;
         }
     }
 
--- SocratesFrTest/CountCoversByDiet/MealChoiceTest.cs
+++ SocratesFrTest/CountCoversByDiet/MealChoiceTest.cs
@@ -7,36 +7,6 @@ namespace SocratesFrTest.CountCoversByDiet
     public class MealChoiceTest
     {
         [Test]
-        public void Should_Count_All_Vegetarian_Meal()
-        {
-            Check.That(new MealChoice().GetVegetarianMeal()).Equals(1);
-        }
-
-        [Test]
-        public void Should_Count_All_Vegan_Meal()
-        {
-            Check.That(new MealChoice().GetVeganMeal()).Equals(1);
-        }
-
-        [Test]
-        public void Should_Count_All_Pescatarian_Meal()
-        {
-            Check.That(new MealChoice().GetPescatarianMeal()).Equals(1);
-        }
-
-        [Test]
-        public void Should_Count_All_Vegetarian_Meal_For_Friday_Lunch()
-        {
-            Check.That(new MealChoice().GetVegetarianMeal(MealTime.FridayLunch)).Equals(1);
-        }
-
-        [Test]
-        public void Should_Count_All_Vegan_Meal_For_Sunday_Lunch()
-        {
-            Check.That(new MealChoice().GetVeganMeal(MealTime.SundayLunch)).Equals(1);
-        }
-
-        [Test]
         public void Should_Count_All_Covers_For_A_Meal_Time_And_Diet()
         {
             Check.That(new MealChoice().CountCovers(MealTime.SundayLunch, Diet.Vegan)).Equals(1);
