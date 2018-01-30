535f27806830d327df73bf16f7a28f9212373768
Unknown <ebstein.bruno@gmail.com>
2018-01-30 13:53:48 +0100

    Add: meal choice should count all vegetarian meal

---
+++ SocratesFr/CountCoversByDiet/MealChoice.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SocratesFr.CountCoversByDiet
+{
+    public class MealChoice
+    {
+        public MealChoice()
+        {
+        }
+
+        public int GetVegetarianMeal()
+        {
+            return 1;
+        }
+    }
+}
---
+++ SocratesFrTest/CountCoversByDiet/MealChoiceTest.cs
@@ -0,0 +1,15 @@
+﻿using NFluent;
+using NUnit.Framework;
+using SocratesFr.CountCoversByDiet;
+
+namespace SocratesFrTest.CountCoversByDiet
+{
+    public class MealChoiceTest
+    {
+        [Test]
+        public void Should_Count_All_Vegetarian_Meal()
+        {
+            Check.That(new MealChoice().GetVegetarianMeal()).Equals(1);
+        }
+    }
+}
