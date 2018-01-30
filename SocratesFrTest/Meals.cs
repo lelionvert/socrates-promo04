ad56a51d75d5d94d938fdeb70c016c3a5d433df4
Anthony <anthony.nizan@gmail.com>
2018-01-30 10:42:19 +0100

    Add: Meals by date with one covers

--- SocratesFrTest/Meals.cs
+++ SocratesFrTest/Meals.cs
@@ -1,13 +1,22 @@
 ﻿using System;
+using NUnit.Framework;
 using SocratesFr;
 
 namespace SocratesFrTest
 {
     internal class Meals
     {
+        private Diets diets;
+
         public int Count(DateTime mealTime, Diet diet)
         {
+            if(diets!= null) return diets.CountBy(diet);
             return 0;
+        }
+
+        public void Add(Diets diets, DateTime mealTime)
+        {
+            this.diets = diets;
         }
     }
 }
--- SocratesFrTest/MealsTest.cs
+++ SocratesFrTest/MealsTest.cs
@@ -16,5 +16,17 @@ namespace SocratesFrTest
             int count = meals.Count(new DateTime(2018, 1, 25, 12, 0, 0), Diet.VEGAN);
             Check.That(count).IsEqualTo(0);
         }
+
+        [Test]
+        public void One_Diets_For_One_Date_Should_Give_One_Vegan_Covers()
+        {
+            Meals meals = new Meals();
+            DateTime mealTime = new DateTime(2018, 1, 25, 12, 0, 0);
+            Diets diets = new Diets();
+            diets.Add(Diet.VEGAN);
+            meals.Add(diets,mealTime);
+            int count = meals.Count(mealTime, Diet.VEGAN );
+            Check.That(count).IsEqualTo(1);
+        }
     }
 }
