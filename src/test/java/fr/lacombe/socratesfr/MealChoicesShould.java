e087079716b5c1f44d2ff31501dcbd6f2bae505d
Gabou <g.delarochette@live.fr>
2018-02-01 16:35:19 +0100

    Repaired test stage that was failing in build

--- src/test/java/fr/lacombe/socratesfr/MealChoicesShould.java
+++ src/test/java/fr/lacombe/socratesfr/MealChoicesShould.java
@@ -2,7 +2,8 @@ package fr.lacombe.socratesfr;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class MealChoicesShould {
     @Test
--- src/test/java/fr/lacombe/socratesfr/StayTest.java
+++
@@ -1,7 +0,0 @@
-package fr.lacombe.socratesfr;
-
-import static org.junit.Assert.*;
-
-public class StayTest {
-
-}
